ef42b935aaf455bad5d2167862d706243e93be42
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-11-30 18:45:21 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./component/table";
+import Control from "./component/control";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column m-5 fs-10">
+            <Control/>
+            <Table/>
+        </div>
+    );
 }
+
 
 export default App;
